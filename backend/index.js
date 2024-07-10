a5ad39752c0599c936bf4f7ab28717ff7940fb5e
Dhananjayhub <dkjha444@gmail.com>
2024-07-10 22:52:36 +0530

    [Feature]: Email send

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./database/db.js";
-const app = express();
-const port = process.env.PORT || 5000;
+
 dotenv.config();
+
+
+const app = express();
+
+app.use(express.json());   //using middleware
+
+const port = process.env.PORT ;
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+import userRoutes from "./routes/user.js";
+app.use("/api", userRoutes);
+
+
+
 app.listen(port, () => {
     console.log(`     Server is running on port http://localhost:${port} `);
-    connectDB
+    connectDB();
 });
