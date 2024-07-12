6e68656c4aa249dbfb256d50697b08324e9633be
Dhananjayhub <dkjha444@gmail.com>
2024-07-12 23:33:32 +0530

    [feature] : backend complete with razorpay

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./database/db.js";
+import Razorpay from "razorpay";
+// import cors from "cors";
 
 dotenv.config();
+
+
+export const instance = new Razorpay({
+    key_id: process.env.Razorpay_Key,
+    key_secret: process.env.Razorpay_Secret,
+  });
 
 
 const app = express();
@@ -16,7 +24,16 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.use("/uploads", express.static("uploads"));
+
+
 import userRoutes from "./routes/user.js";
+import courseRoutes from "./routes/course.js";
+import adminRoutes from "./routes/admin.js";
+
+
+app.use("/api", courseRoutes);
+app.use("/api", adminRoutes);
 app.use("/api", userRoutes);
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { register , verifyUser } from "../controllers/user.js";
+import { register ,  myProfile, verifyUser, loginUser } from "../controllers/user.js";
+
+import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
 router.post("/user/register",register);
 router.post("/user/verify",verifyUser);
+router.post("/user/login", loginUser);
+router.get("/user/me", isAuth, myProfile);
 
 export default router;
