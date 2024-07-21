1f1e55384a3bb7a665124d9dd18b2631de126127
Dhananjayhub <dkjha444@gmail.com>
2024-07-21 10:59:47 +0530

    [feature]: Add course study and leacture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,11 @@ import About from "./pages/about/About";
 import Courses from "./pages/courses/Courses";
 import Loading from "./components/loading/Loading";
 import Account from "./pages/account/Account";
+import CourseDescription from "./pages/coursedescription/CourseDescription";
+import PaymentSuccess from "./pages/paymentsuccess/PaymentSuccess";
+import Dashbord from "./pages/dashbord/Dashbord";
+import CourseStudy from "./pages/coursestudy/CourseStudy";
+import Lecture from "./pages/lecture/Lecture";
 import { UserData } from "./context/UserContext";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -32,6 +37,30 @@ const App = () => {
 
           <Route path="/register" element={isAuth ? <Home /> : <Register />} />
           <Route path="/verify" element={isAuth ? <Home /> : <Verify />} />
+
+          <Route
+              path="/course/:id"
+              element={isAuth ? <CourseDescription user={user} /> : <Login />}
+            />
+            <Route
+              path="/payment-success/:id"
+              element={isAuth ? <PaymentSuccess user={user} /> : <Login />}
+            />
+
+<Route
+              path="/:id/dashboard"
+              element={isAuth ? <Dashbord user={user} /> : <Login />}
+            />
+            <Route
+              path="/course/study/:id"
+              element={isAuth ? <CourseStudy user={user} /> : <Login />}
+            />
+
+              <Route
+              path="/lectures/:id"
+              element={isAuth ? <Lecture user={user} /> : <Login />}
+            />
+
         </Routes>
 
         <Footer />
